018f8f26abed8a481708e6856ba5f494a9b449c0
Jackson <jviana116@gmail.com>
2023-01-16 16:28:11 -0300

    refactor: trocando a lib requests para playwright

--- crawler.py
+++ crawler.py
@@ -1,42 +1,40 @@
-import requests
-from parsel import Selector
+from playwright.sync_api import sync_playwright
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-def get_page(url):
-    try:
-        response = requests.get(url)
-    except requests.exceptions.RequestException:
-        return None
-    else:
-        return response.text
 
+def get_laptops(p):
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto(
+        'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops')
 
-def extract_data(html):
-    selector = Selector(html)
     laptop_data = []
-    for laptop in selector.css('div.thumbnail'):
-        title = laptop.css('a.title::text').get()
-        price = laptop.css('h4.pull-right::text').get()
-        description = laptop.css('p::text').get()
-        rating = laptop.css('p.pull-right::text').get()
+    laptops = page.query_selector_all('div.thumbnail')
+    for laptop in laptops:
+        title = laptop.query_selector('a.title').inner_text()
+        price = laptop.query_selector('h4.pull-right').inner_text()
+        description = laptop.query_selector('p').inner_text()
+        rating = laptop.query_selector('p.pull-right').inner_text()
         laptop_data.append({
             'title': title,
             'price': float(price[1:]),
             'description': description,
-            'rating': rating
+            'rating': rating,
         })
-    lenovo_laptops = list(filter(lambda x: 'Lenovo' in x['title'], laptop_data))
+    browser.close()
+    lenovo_laptops = list(
+        filter(lambda x: 'Lenovo' in x['title'], laptop_data))
     sorted_list = sorted(lenovo_laptops, key=lambda x: x['price'])
     return sorted_list
 
+
 @app.route('/laptops', methods=['GET'])
-def get_laptops():
-    html_text = get_page(
-        'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops')
-    laptop_data = extract_data(html_text)
-    return jsonify(laptop_data)
+def main():
+  with sync_playwright() as p:
+      result = get_laptops(p)
+  return jsonify(result)
 
 
 if __name__ == '__main__':
